decd40cc9d2a0acc1865bbade0beccb1273812cd
sonny <sonny.7n@gmail.com>
2025-07-22 15:20:52 -0500

    Add Spring Boot skeleton

---
+++ src/main/java/com/sonny/numbers_machine/NumbersMachineApplication.java
@@ -0,0 +1,13 @@
+package com.sonny.numbers_machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbersMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonny/numbers_machine/NumbersMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonny.numbers_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbersMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
