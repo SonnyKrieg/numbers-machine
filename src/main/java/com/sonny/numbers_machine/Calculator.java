d8cd0b0a6923042601d7d9d79056b655eb120d36
sonny <sonny.7n@gmail.com>
2025-07-22 15:32:47 -0500

    Add sum logic, controller and unit test

---
+++ src/main/java/com/sonny/numbers_machine/Calculator.java
@@ -0,0 +1,12 @@
+package com.sonny.numbers_machine;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/com/sonny/numbers_machine/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.sonny.numbers_machine;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        Assertions.assertEquals(5, calculator.sum(2, 3));
+    }
+
+
+}
